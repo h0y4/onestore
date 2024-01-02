9f58cc9833c44e4ff037335d7cd800a236404fd6
h0y4 <154441659+h0y4@users.noreply.github.com>
2024-01-02 23:48:24 +0900

    상단 네비 js 추가 및 slick js 추가해서 company 하단 유튜브 영상 슬라이드

--- js/company.js
+++ js/company.js
@@ -1,22 +1,35 @@
 $(function () {
+  let nav = $("#nav");
 
   $('#link').click(function () {
-    var src = src='https://www.youtube.com/embed/eIG4nIsfrmE?si=GkMJF8Uoo4c2O0Ml title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+    var src = src = 'https://www.youtube.com/embed/eIG4nIsfrmE?si=GkMJF8Uoo4c2O0Ml title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
     $('#videoModal').modal('show');
     $('#videoModal iframe').attr('src', src);
   });
 
-
   $(window).scroll(function () {
-    let nav = $("#nav");
-
     let scTop = $(window).scrollTop();
-    if (scTop > 1) {
+    if (scTop > 2) {
       nav.addClass("nav-fixed")
     }
     if (scTop == 0) {
       nav.removeClass("nav-fixed")
     }
   })
+  nav.mouseover(function () {
+    nav.addClass("on")
+  })
+  nav.mouseout(function () {
+    nav.removeClass("on")
+  })
 
+  $('.slick-list').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow:$('.slick-next'),
+    prevArrow:$('.slick-prev'),
+  });
 })
+
